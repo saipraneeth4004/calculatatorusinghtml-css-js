85eff52d8df46b24711b40dca389dc5a5dd661fb
2300031549@kluniversity.in <2300031549@kluniversity.in>
2025-03-02 09:26:55 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,23 @@
+
+const input = document.getElementById('input');
+const buttons = document.querySelectorAll('button');
+
+buttons.forEach(button => {
+    button.addEventListener('click', () => {
+        const buttonValue = button.textContent;
+
+        if (buttonValue === 'AC') {
+            input.value = '';
+        } else if (buttonValue === 'DEL') {
+            input.value = input.value.slice(0, -1);
+        } else if (buttonValue === '=') {
+            try {
+                input.value = eval(input.value);
+            } catch (error) {
+                input.value = 'Error';
+            }
+        } else {
+            input.value += buttonValue;
+        }
+    });
+});
